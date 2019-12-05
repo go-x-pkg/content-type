76098504b1c38f8bf62a716b28f4836e5e95e7ef
Novel Corpse <romandafe94@gmail.com>
2019-12-05 22:10:17 +0300

    refs #1; added various content types;

--- contenttype.go
+++ contenttype.go
@@ -10,16 +10,56 @@ const (
 
 	// YAML / Yet Another Markup Language
 	YAML
+
+	// TSV / Tab Separated Values
+	TSV
+
+	// CSV / Comma-Separated Values
+	CSV
+
+	// XML / eXtensible Markup Language
+	XML
+
+	// HTML / HyperText Markup Language
+	HTML
+
+	// Protobuf / Protocol Buffers
+	// see: https://tools.ietf.org/html/draft-rfernando-protocol-buffers-00
+	Protobuf
+
+	// MsgPack / Message Pack
+	MsgPack
+
+	// M3U8 / M3U8 files are the basis for the HTTP Live Streaming (HLS)
+	/* see:
+	https://en.wikipedia.org/wiki/M3U#Internet_media_types
+	https://developer.apple.com/library/archive/documentation/NetworkingInternet/Conceptual/StreamingMediaGuide/DeployingHTTPLiveStreaming/DeployingHTTPLiveStreaming.html
+	*/
+	M3U8
 )
 
 var codecText = map[Kind]string{
-	JSON: "json",
-	YAML: "yaml",
+	JSON:     "json",
+	YAML:     "yaml",
+	TSV:      "tsv",
+	CSV:      "csv",
+	XML:      "xml",
+	HTML:     "html",
+	Protobuf: "protobuf",
+	MsgPack:  "msgpack",
+	M3U8:     "m3u8",
 }
 
 var contentTypeText = map[Kind]string{
-	JSON: "application/json",
-	YAML: "application/x-yaml",
+	JSON:     "application/json",
+	YAML:     "application/x-yaml",
+	XML:      "application/xml",
+	Protobuf: "application/protobuf",
+	MsgPack:  "application/x-msgpack",
+	M3U8:     "application/vnd.apple.mpegurl",
+	TSV:      "text/tab-separated-values",
+	CSV:      "text/csv",
+	HTML:     "text/html",
 }
 
 // String impl Stringer for Kind
